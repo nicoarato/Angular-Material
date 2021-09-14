410f83c9aacb3078ef336ee6d3933be031aadcfa
narato <nicolas.arato@i2t-sa.com.ar>
2021-09-14 13:25:37 -0300

    Proyecto con material funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { EncuestaComponent } from './encuesta/encuesta.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncuestaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
